2dc41227511b04beee50d8ef1b9e8fe1145576c3
jayjariwala <jay.jariwala@hotmail.com>
2017-06-03 17:05:55 -0400

    Route parameters

--- basic-routing/src/app/about/about.component.ts
+++ basic-routing/src/app/about/about.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from "@angular/router";
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  ninja :string;
+  constructor(private route:ActivatedRoute)
+  {
+    this.ninja = route.snapshot.params['ninja'];
+  }
 
   ngOnInit() {
   }
--- basic-routing/src/app/app.routes.ts
+++ basic-routing/src/app/app.routes.ts
@@ -4,5 +4,5 @@ import { HomeComponent }  from "./home/home.component";
 
 export const routes: Routes = [
   { path : '', component:HomeComponent },
-  { path : 'about', component:AboutComponent}
+  { path : 'about/:ninja', component: AboutComponent }
 ];
